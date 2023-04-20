c300cbbe8ea46dc95d35ac0d7b732313b90d7e60
vickyyg <viicjygaida@gmail.com>
2023-04-20 11:46:46 -0300

    jajajaj

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { NavBar } from "./components/navbar/navbar";
+import Titulo from "./components/titulo/titulo";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Titulo />
+    </div>
+  );
+}
+
+export default App;
